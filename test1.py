34dacc0e4e2e2aca00e6884179f2f217bf3baffd
Ruchira Edirisnghe <ruchiedi1258@gmail.com>
2023-03-10 12:12:45 +0530

    Changes

--- test1.py
+++ test1.py
@@ -1,7 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 10 01:41:32 2023
+# PhURL - Web Based Phishing URL Detection & Learning Platform
+# Created By - Ruchira Edirisinghe
+# Plymouth Final Year Project
 
+"""
 @author: ruchi_lb7de3w
 """
 
+# Importing Data
+import numpy as np
+import pandas as pd
+
+# loading dataset
+
+dataset = pd.read_csv ('url_data.csv')
