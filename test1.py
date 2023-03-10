cf6d0f9e0c86e028b26fbcbf8872838a1b16814f
Ruchira Edirisnghe <ruchiedi1258@gmail.com>
2023-03-10 13:10:12 +0530

    preprocessing

--- test1.py
+++ test1.py
@@ -1,6 +1,6 @@
-# PhURL - Web Based Phishing URL Detection & Learning Platform
-# Created By - Ruchira Edirisinghe
-# Plymouth Final Year Project
+#### PhURL - Web Based Phishing URL Detection & Learning Platform ####
+#### Created By - Ruchira Edirisinghe ####
+#### Plymouth Final Year Project - 2023 ####
 
 """
 @author: ruchi_lb7de3w
@@ -11,5 +11,16 @@ import numpy as np
 import pandas as pd
 
 # loading dataset
+dataset = pd.read_csv ('phishing-dataset-variation.csv')
+print(dataset.describe())
 
-dataset = pd.read_csv ('url_data.csv')
+# Create dependent & independent variable vectors
+
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,-1].values
+print(x)
+
+# handle missing data
+
+# count the number of missing values in each column
+print(dataset.isnull().sum())
