bcc1e8028a654686f361a11240daddfa236ca4ab
Ruchira Edirisinghe <ruchiedi1258@gmail.com>
2023-03-23 01:39:55 +0530

    Updated Build Models

--- model-build/build_rf_model.py
+++ model-build/build_rf_model.py
@@ -17,11 +17,11 @@ import seaborn as sns
 dataset = pd.read_csv('../preprocessed_dataset.csv')
 print(dataset.describe())
 
-# remove first 2 columns
-X = dataset.iloc[:,2:].values
+# remove first column
+X = dataset.iloc[:,1:].values
 
-# assign to 2nd column
-y = dataset.iloc[:,1].values 
+# assign to 1st column
+y = dataset.iloc[:,0].values 
 
 # Encoding categorical variables
 from sklearn.preprocessing import LabelEncoder
@@ -45,44 +45,43 @@ y_pred_rf = rf.predict(X_test)
 # Model Evaluation
 trainAS = rf.score(X_train, y_train)
 val_accurcy = rf.score(X_test, y_test)
-val_loss = metrics.log_loss(y_test, y_pred_rf)
+# accuracy = metrics.accuracy_score(y_test, y_pred_rf)
 precision = metrics.precision_score(y_test, y_pred_rf, average='weighted')
 f1 = metrics.f1_score(y_test, y_pred_rf, average='weighted')
 r2 = metrics.r2_score(y_test, y_pred_rf)
 
 # Output
 print("\nAccuracy on the train dataset: %.4f%%" % (trainAS * 100))
-print("Validation Accuracy: %.4f%%" % (trainAS * 100))
-print("Validation Loss: %.4f%%" % (trainAS * 100))
-print("Precision: %.4f%%" % (trainAS * 100))
-print("F1 Score: %.4f%%" % (trainAS * 100))
-print("R2 Score: %.4f%%" % (trainAS * 100))
+print("Validation Accuracy: %.4f%%" % (val_accurcy * 100))
+# print("Accuracy: %.4f%%" % (accuracy * 100))
+print("Precision: %.4f%%" % (precision * 100))
+print("F1 Score: %.4f%%" % (f1 * 100))
+print("R2 Score: %.4f%%" % (r2 * 100))
 
 
 
 
 
 
+# ## Figure Generate 13: Confusion Matrix of Random Forest Model
 
-## Figure Generate 13: Confusion Matrix of Random Forest Model
-
-cm = confusion_matrix(y_test, y_pred_rf)
-cm_df = pd.DataFrame (cm,
-                      index = ['benign', 'defacement', 'phishing', 'malware'],
-                      columns = ['benign', 'defacement', 'phishing', 'malware'])
-plt.figure(figsize=(8,6))
-sns.heatmap(cm_df, annot=True, fmt=".1f")
-plt.title('Confusion Matrix')
-plt.ylabel('Actal Values')
-plt.xlabel('Predicted Values')
-plt.show()
-
+# cm = confusion_matrix(y_test, y_pred_rf)
+# cm_df = pd.DataFrame (cm,
+#                       index = ['benign', 'defacement', 'phishing', 'malware'],
+#                       columns = ['benign', 'defacement', 'phishing', 'malware'])
+# plt.figure(figsize=(8,6))
+# sns.heatmap(cm_df, annot=True, fmt=".1f")
+# plt.title('Confusion Matrix')
+# plt.ylabel('Actal Values')
+# plt.xlabel('Predicted Values')
+# plt.show()
 
 
 
-## Figure Generate 14: Feature importances of Random Forest Model
 
-feat_importances = pd.Series(rf.feature_importances_, index=X_train.columns)
-feat_importances.sort_values().plot(kind="barh",figsize=(10,6))
+# ## Figure Generate 14: Feature importances of Random Forest Model
+
+# feat_importances = pd.Series(rf.feature_importances_, index=X_train.columns)
+# feat_importances.sort_values().plot(kind="barh",figsize=(10,6))
 
 
